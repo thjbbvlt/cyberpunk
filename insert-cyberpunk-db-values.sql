768d2e4908e5043e0ab32031a382a91acb3a73e0
thjbbvlt <thibault2@ik.me>
2022-11-21 20:14:33 +0100

    nouveau fichier : insert-cyberpunk-db-values.sql

---
+++ insert-cyberpunk-db-values.sql
@@ -0,0 +1,103 @@
+insert into pays values ('us');
+insert into langue values ('eng');
+
+/* organisation */
+
+insert into organisation values (1, 'Doubleday', 'usa', 1897);
+
+insert into organisation values (2, 'dell publishing', 'usa', 1921);
+
+insert into organisation values (3, 'arbor house', 'usa', 1969-1988);
+
+insert into organisation values (4, 'Bantam Books', 'usa', 1945);
+
+insert into organisation values (5, 'Bantam Books', 'usa', 1945);
+
+insert into organisation values (6, 'simon & schuster', 'usa', 1924);
+
+insert into organisation values (7, 'ace books', 'usa', 1952);
+
+insert into organisation values (8, 'orbit books', 'uk', 1974);
+
+insert into organisation values (9, 'crown publishing group', 'usa', 1933);
+
+Do Androids Dream of Electric Sheep?
+print
+usa
+eng
+Doubleday
+1968
+1992 (2021 in later edition)
+
+The Girl Who Was Plugged In
+print
+usa
+eng
+Doubleday
+1973
+null
+
+True Names
+print
+usa
+eng
+dell publishing
+1981
+null
+
+Islands in the Net
+print
+usa
+eng
+arbor house
+1988
+2023
+
+Snow Crash
+print
+usa
+eng
+Bantam Books
+1992
+21st century
+
+virtual light
+print
+usa
+eng
+bantam spectra
+1993
+2006
+
+heavy weather
+print
+usa
+eng
+bantam spectra
+1994
+2031
+
+River of Gods
+print
+uk
+eng
+simon & schuster
+2004
+2047
+
+Glasshouse
+uk / us
+orbit / ace
+2006
+eng
+27th century
+
+Ready Player One
+print
+usa
+eng
+crown publishing group
+2011
+2040
+
+

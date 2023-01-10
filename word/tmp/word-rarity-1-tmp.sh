3dc0fa92eb4293248674bb62889ce6a2de036240
thjbbvlt <thibault2@ik.me>
2023-01-10 19:59:21 +0100

    nouveau fichier : word/tmp/word-rarity-1-tmp.sh 	nouveau fichier : word/tmp/word-rarity-2-tmp.sh 	nouveau fichier : word/word_lexical_class.py 	nouveau fichier : word/word_rarity.sh

---
+++ word/tmp/word-rarity-1-tmp.sh
@@ -0,0 +1,10 @@
+python3 word_lexical_class.py 'do androids dream of electric sheep?' 1001
+python3 word_lexical_class.py 'the girl who was plugged in' 1002
+python3 word_lexical_class.py 'true names' 1003
+python3 word_lexical_class.py 'islands in the net' 1004
+python3 word_lexical_class.py 'snow crash' 1005
+python3 word_lexical_class.py 'virtual light' 1006
+python3 word_lexical_class.py 'heavy weather' 1007
+python3 word_lexical_class.py 'river of gods' 1008
+python3 word_lexical_class.py 'glasshouse' 1009
+python3 word_lexical_class.py 'ready player one' 1010
---
+++ word/word_rarity.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+grep 'into manifestation' values.sql | sed "s/insert into manifestation values (\([^,]*\), \([^,]*\), .*/python3 word_lexical_class.py \2 \1/g" | tr 'A-Z' 'a-z' > ./tmp/word-rarity-1-tmp.sh
+# grep: pour sélectionner les lignes comportant des titres
+# sed: pour sélectionner, dans ces lignes, les titres (\2) et les keys des manifestations (\1); et pour transformer le résultat en commandes.
+# tr: pour enlever les majuscules (sinon wordnet pense qu'il s'agit de noms propres)
+# j'exporte ensuite dans un fichier temporaire
+
+bash ./tmp/word-rarity-1-tmp.sh > ./tmp/word-rarity-2-tmp.sh
+
+sed -i "s/'VB'/-famlv/g" ./tmp/word-rarity-2-tmp.sh
+sed -i "s/'VB.'/-famlv/g" ./tmp/word-rarity-2-tmp.sh
+sed -i "s/'NN'/-famln/g" ./tmp/word-rarity-2-tmp.sh
+sed -i "s/'NN.'/-famln/g" ./tmp/word-rarity-2-tmp.sh
+sed -i "s/'JJ'/-famla/g" ./tmp/word-rarity-2-tmp.sh
+sed -i "s/'JJ.'/-famla/g" ./tmp/word-rarity-2-tmp.sh
+sed -i "s/'RB'/-famlr/g" ./tmp/word-rarity-2-tmp.sh
+sed -i "s/'RB.'/-famlr/g" ./tmp/word-rarity-2-tmp.sh
+
+cat ./tmp/word-rarity-2-tmp.sh
+
+# maintenant: il faut utiliser wordnet!
+
+# et pour plus tard: récupérer les titres depuis la db directement (comme ça je peux aussi utiliser importation dbpedia!).
+
+# et les wordnet hyperonyme?
